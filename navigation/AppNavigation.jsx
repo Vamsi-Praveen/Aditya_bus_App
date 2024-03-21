06cab76c2e2e084cabdd6626199d52e0149ab46c
Vamsi-Praveen <21A91A05G5@aec.edu.in>
2024-03-21 20:56:58 +0530

    completed

--- navigation/AppNavigation.jsx
+++ navigation/AppNavigation.jsx
@@ -3,19 +3,28 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { AuthProvider } from '../context/AuthContext';
+import BusContext from '../context/BusContext';
+import QRCodeScanner from '../screens/QRCode';
+import Scanner from '../screens/Scanner';
 
 const Stack = createNativeStackNavigator();
 const AppNavigation = () => {
     return (
-        <SafeAreaView>
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name='Home' component={HomeScreen} />
-                    <Stack.Screen name='Login' component={LoginScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </SafeAreaView>
+        <NavigationContainer>
+            <AuthProvider>
+                <BusContext>
+                    <Stack.Navigator initialRouteName='Login' screenOptions={{
+                        headerShown: false
+                    }}>
+                        <Stack.Screen name='Home' component={HomeScreen} />
+                        <Stack.Screen name='Login' component={LoginScreen} />
+                        <Stack.Screen name='Scan' component={QRCodeScanner} />
+                        <Stack.Screen name='Scanner' component={Scanner} />
+                    </Stack.Navigator>
+                </BusContext>
+            </AuthProvider>
+        </NavigationContainer>
     )
 }
 
